8fde65eb368b0ae159f468c06f8838f43b6bb6a2
Mrjinliang555 <yicuoguoliang@163.com>
2018-04-06 22:20:14 +0800

    首页基本功能完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,7 +1,15 @@
 /**
  * Created by toshiba on 2018/4/6.
  */
-
+//$.ajax({
+//  url: "/employee/checkRootLogin",
+//  success: function(info){
+//    console.log(info);
+//    if( info.error ){
+//      location.href = "login.html"
+//    }
+//  }
+//})
 //nprogress 事件设置
 $(function(){
 
@@ -15,5 +23,40 @@ $(function(){
     setTimeout(function(){
       NProgress.done();
     },400)
+  })
+})
+
+//分类切换
+
+$(".classify").click(function(){
+  $(this).next().stop().slideToggle();
+})
+
+//菜单显示隐藏
+
+$(".menu_btn").click(function(){
+
+$(".lt_aside").toggleClass("tohidden");
+$(".lt_main").toggleClass("tohidden");
+$(".main_top").toggleClass("tohidden");
+
+})
+
+//退出登录
+$(".logout_btn").click(function(){
+
+  $('#myModal').modal();
+
+})
+$(".btn_logout").click(function(){
+  //alert(1)
+  $.ajax({
+    url: "/employee/employeeLogout",
+    success: function(info){
+      //console.log(info);
+      if( info.success ){
+        location.href = "login.html"
+      }
+    }
   })
 })
